b4b0c00521288c073f6643032ad230405a70cbb6
Zahid <i200469@nu.edu.pk>
2024-03-05 23:02:44 +0500

    Refactor card data fetching in CardWrapper component

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -16,10 +16,10 @@ const iconMap = {
 
 export default async function CardWrapper() {
   const {
+    numberOfCustomers,
+    numberOfInvoices,
     totalPaidInvoices,
     totalPendingInvoices,
-    numberOfInvoices,
-    numberOfCustomers,
   } = await fetchCardData();
   return (
     <>
